e12ceba38e40d98612af8e9e7d3c12a3ec499e56
Kolanu-nikshith <32254529+Kolanu-nikshith@users.noreply.github.com>
2022-02-26 12:53:12 +0530

    lm75, mq2, mq7

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, flash, redirect,url_for, jsonify, session 
+from flask import Flask, render_template, request, flash, redirect,url_for, jsonify, session
 from flask import Response,send_file
 
 app = Flask(__name__)
 
-import rds_db as db
+import rds_db as dat
+
+
+@app.route('/',methods = ['GET','POST'])
+def inde():
+    return "Hello world"
+
 
 @app.route('/<textdata>',methods = ['GET','POST'])
 def index(textdata):
-    details = db.get_details()
+    print("printing post data")
+    print(textdata)
+
+    details = dat.get_lm75()
+    print("***printing lm75 data from db***")
+    print(details)
+
+    details = dat.get_mq2()
+    print("***printing mq2 data from db***")
+    print(details)
+
+    details = dat.get_mq7()
+    print("***printing mq7 data from db***")
     print(details)
     return "Data received to aws server"
 
 
-# @app.route('/insert',methods = ['post'])
-# def insert():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         gender = request.form['optradio']
-#         comment = request.form['comment']
-#         db.insert_details(name,email,comment,gender)
-#         details = db.get_details()
-#         print(details)
-#         for detail in details:
-#             var = detail
-#         return render_template('index.html',var=var)
-
-
 if __name__ == "__main__":
-    
     app.run(debug=True)
--- rds_db.py
+++ rds_db.py
@@ -1,22 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul 25 13:34:18 2020
-
-@author: hp
-"""
-
 
 
 import pymysql
-import aws_credentials as rds
-
 table_created=1
 
 conn = pymysql.connect(
         host= "test.cq7sapjb7scv.ap-south-1.rds.amazonaws.com", #endpoint link
         port = 3306, # 3306
         user = "admin", # admin
-        password = "qwertyuiop" #rds.password, #adminadmin
+        password = "qwertyuiop", #rds.password, #adminadmin
         db = "test", #test
         )
 
@@ -24,20 +15,83 @@ conn = pymysql.connect(
 if table_created:
     table_created=0
     cursor=conn.cursor()
-    create_table = """create table Details (lm75 varchar(1024))"""
-    cursor.execute(create_table)
-    cur=conn.cursor()
-    cur.execute("INSERT INTO Details (lm75)) VALUES (%s)", (lm75))
+    lm75 = "lm75"
+    mq2 = "mq2"
+    mq7 = "mq7"
+    create_lm75 = """create table IF NOT EXISTS lm75 (id int NOT NULL AUTO_INCREMENT,value varchar(254), inserttime DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id));"""
+    create_mq2 =  """create table IF NOT EXISTS mq2 (id int NOT NULL AUTO_INCREMENT,value varchar(254), inserttime DATETIME DEFAULT CURRENT_TIMESTAMP,  PRIMARY KEY (id));"""
+    create_mq7 =  """create table IF NOT EXISTS mq7 (id int NOT NULL AUTO_INCREMENT,value varchar(254), inserttime DATETIME DEFAULT CURRENT_TIMESTAMP,  PRIMARY KEY (id));"""
+    cursor.execute(create_lm75)
+    cursor.execute("INSERT INTO lm75 (value) VALUES (%s)", (lm75))
+
+    cursor.execute(create_mq2)
+    cursor.execute("INSERT INTO mq2 (value) VALUES (%s)", (mq2))
+
+    cursor.execute(create_mq7)
+    cursor.execute("INSERT INTO mq7 (value) VALUES (%s)", (mq7))
+
     conn.commit()
 
 
-def insert_details(name,email,comment,gender):
-    cur=conn.cursor()
-    cur.execute("INSERT INTO Details (lm75)) VALUES (%s)", (lm75))
-    conn.commit()
 
-def get_details():
-    cur=conn.cursor()
-    cur.execute("SELECT *  FROM Details")
-    details = cur.fetchall()
+def get_lm75():
+    try:
+        cur=conn.cursor()
+        cur.execute("SELECT *  FROM lm75")
+        details = cur.fetchall()
+    except:
+        print("fetching lm75 details failed")
+        return "failed"
     return details
+
+def get_mq2():
+    try:
+        cur=conn.cursor()
+        cur.execute("SELECT *  FROM mq2")
+        details = cur.fetchall()
+    except:
+        print("fetching mq2 details failed")
+        return "failed"
+    return details
+
+def get_mq7():
+    try:
+        cur=conn.cursor()
+        cur.execute("SELECT *  FROM mq7")
+        details = cur.fetchall()
+    except:
+        print("fetching mq7 details failed")
+        return "failed"
+    return details
+
+
+def insert_lm75():
+    lm75 = "lm75"
+    try:
+        cur=conn.cursor()            
+        cur.execute("INSERT INTO lm75 (value) VALUES (%s)", (lm75))
+        conn.commit()
+    except:
+        return "Inserting into lm75 failed"
+    return "Inserting into lm75 success"
+
+def insert_mq2():
+    mq2 = "mq2"
+    try:            
+        cur=conn.cursor()
+        cur.execute("INSERT INTO mq2 (value) VALUES (%s)", (mq2))
+        conn.commit()
+    except:
+        return "Inserting into mq2 failed"
+    return "Inserting into mq2 success"
+
+def insert_mq7():
+    mq7 = "mq7"
+    try:            
+        cur=conn.cursor()
+        cur.execute("INSERT INTO mq7 (value) VALUES (%s)", (mq7))
+        conn.commit()
+    except:
+        return "Inserting into mq7 failed"
+    return "Inserting into mq7 success"
+
